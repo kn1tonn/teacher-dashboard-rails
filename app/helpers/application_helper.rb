2f8ee2cd1ce42dcefbcfe0b7ff3779aaf929a2ce
KEN-4 <ssstututusss617@gmail.com>
2025-11-01 18:18:07 +0900

    Brakeman対処

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+require "uri"
 module ApplicationHelper
+  def safe_external_url(raw_url)
+    return if raw_url.blank?
+    url = raw_url.to_s.strip
+    uri = URI.parse(url)
+    return url if uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+  rescue URI::InvalidURIError
+    nil
+  end
+endmodule ApplicationHelper
 end
